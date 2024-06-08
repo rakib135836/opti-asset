20ce352f8412bdaacfa67cef4beee9209e1c2a4f
rakib <rakib135836@gmail.com>
2024-06-08 21:41:43 +0600

    add asset form done

--- src/Pages/Dashboard/Hr/AddAsset/AddAsset.jsx
+++ src/Pages/Dashboard/Hr/AddAsset/AddAsset.jsx
@@ -1,9 +1,45 @@
 
 
+
 const AddAsset = () => {
     return (
         <div>
-            <h1>add asset</h1>
+          <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold text-center">Add an Asset</h1>
+                    </div>
+                    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form  className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Product Name</span>
+                                </label>
+                                <input type="text" name="Name" placeholder="Name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Product Quantity</span>
+                                </label>
+                                <input type="text" name="quantity" placeholder="product quantity" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">product type</span>
+                                </label>
+                                <input type="text" name="etype" placeholder="returnable or non returnable" className="input input-bordered" required />
+                            </div>
+                           
+                            <div className="form-control mt-6">
+                                <button type="submit" className="btn btn-primary">Add</button>
+                            </div>
+                        </form>
+                        
+                    </div>
+                    
+                    
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/Hr/HrHome/HrHome.jsx
+++ src/Pages/Dashboard/Hr/HrHome/HrHome.jsx
@@ -3,7 +3,12 @@
 const HrHome = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>● Pending requests (max: 5 items)
+                ● Top most requested items (max: 4 items)
+                ● Limited Stock items (Quantity less than 10)
+                ● Make a pie chart for the total percentage of returnable items and
+                non-returnable items requested by the employee.
+                ● Add 2 relevant extra section</h1>
         </div>
     );
 };
