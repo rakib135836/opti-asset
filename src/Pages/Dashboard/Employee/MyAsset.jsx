d9012f4754be0ded8e6f4774f4605b62ab198339
rakib <rakib135836@gmail.com>
2024-09-09 00:57:13 +0600

    my assets

--- src/Pages/Dashboard/Employee/MyAsset.jsx
+++ src/Pages/Dashboard/Employee/MyAsset.jsx
@@ -1,9 +1,64 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const MyAsset = () => {
+
+    const { user } = useAuth();
+    const email = user?.email;
+    const axiosSecure = useAxiosSecure();
+
+
+    const {  data: myassets = [], isLoading } = useQuery({
+        queryKey: ['my-asset', email],
+        enabled: !!email,
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/my-asset/${email}`);
+            return res.data;
+        }
+    })
+
+    if (isLoading) {
+        <span className="loading loading-spinner text-accent"></span>
+    }
+
     return (
         <div>
-            my asset
+            {myassets?.length > 0 ? (
+                <>
+                    <h1 className="text-2xl font-bold text-center py-4">My Assets</h1>
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            {/* Table head */}
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Type</th>
+                                    
+                                    <th>Status</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {myassets.map((request) => (
+                                    <tr key={request?._id}>
+
+                                        <td>{request?.asset}</td>
+                                        <td>{request?.assetType}</td>
+                                
+                                        <td>{request?.status}</td>
+
+                                        
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </>
+            ) : (
+                <p>No asset found</p>
+            )}
         </div>
     );
 };
